935a848318c410db9822c47e1033d0b31497bbaa
The Seoul Valeur <hareum1020@gmail.com>
2026-09-17 17:36:46 +0900

    Apply TSV monogram to site logo and browser icons

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,7 @@
 export function Logo() {
   return (
     <a className="logo" href="/" aria-label="The Seoul Valeur home">
-      <span className="logoMark">TSV</span>
+      <img className="logoMark" src="/images/tsv-logo.png" alt="" width={48} height={48} />
       <span className="logoText">
         <span>The Seoul Valeur</span>
         <small>Seoul Beauty & Travel Journal</small>
